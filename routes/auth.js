9b6bb5d6f73b61bfbf2435a37f639aef50147ddc
MostafaAshraf0 <mostafaashraf334@gmail.com>
2024-03-11 15:39:03 +0300

    data Validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,61 @@
 const express = require('express');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
-
-
+const User = require('../models/user');
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post(
+    '/login',
+    [
+    body('email')
+        .isEmail()
+        .withMessage('Please enter a valid email address.')
+        .normalizeEmail(),
+    body('password', 'Password has to be valid.')
+        .isLength({ min: 5 })
+        .isAlphanumeric()
+        .trim()
+    ],
+    authController.postLogin
+  );
 
-router.post('/signup', authController.postSignup);
+router.post('/signup',
+[
+check('email')
+.isEmail()
+.withMessage('Please Enter A Valid Email.')
+.custom((value, {req}) => {
+    // if(value === 'test@test.com') {
+    //     throw new Error('This email address is forbidden.');
+    // }
+    // return true;
+    return User.findOne({email: value})
+    .then(userDoc => {
+    if(userDoc){
+        return Promise.reject('Email already exists.');
+        
+    }
+})
+})
+.normalizeEmail(),
+body(
+    'password',
+    'Pleas enter a password with only numbers and text at least 5 characters.'
+).isLength({min: 5})
+.isAlphanumeric()
+.trim(),
+body('confirmPassword').trim().custom((value, {req})=> {
+    if(value !== req.body.password){
+        throw new Error('Password have to match!')
+    }
+    return true;
+})
+], authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
